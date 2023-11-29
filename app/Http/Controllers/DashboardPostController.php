14c1761bba5a8568157c7757a84b97848e1faefe
Dandywahyudin <dandywahyudin19@gmail.com>
2023-11-29 22:21:21 +0700

    video 21 error gambar

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -3,49 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Posts;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardPostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-
         return view('dashboard.posts.index', [
             'posts' => Posts::where('user_id', auth()->user()->id)->get()
         ]);
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('dashboard.posts.create', [
+            'categories' => Category::all()
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+
+        $validatedData = $request->validate([
+            'title' => 'required|max:255|min:3',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'image' => 'image|file|max:10024',
+            'body' => 'required'
+        ]);
+
+        if ($request->file('image')) {
+            $validatedData['image'] = $request->file('image')->store('posts-images');
+        }
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Posts::create($validatedData);
+
+        return redirect('/dashboard/post')->with('success', 'New Posts has been added!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function show(Posts $post)
     {
         return view('dashboard.posts.show', [
@@ -53,37 +54,59 @@ class DashboardPostController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Posts  $post
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Posts $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Posts  $post
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Posts $post)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|max:255|min:3',
+            'category_id' => 'required',
+            'image' => 'image|file|max:10024',
+            'body' => 'required'
+        ]);
+
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validatedData = $request->validate($rules);
+
+        if ($request->file('image')) {
+            if($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Posts::where('id', $post->id)
+            ->update($validatedData);
+
+        return redirect('/dashboard/posts')->with('success', 'Posts has been updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Posts  $post
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Posts $post)
     {
-        //
+        if($post->Image) {
+            Storage::delete($post->Image);
+        }
+        Posts::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'Posts has been deleted!');
+    }
+
+    public function checkSlug(Request $request)
+    {
+        $slug = SlugService::createSlug(Posts::class, 'slug', $request->title);
+        return response()->json(['slug' => $slug]);
     }
 }
